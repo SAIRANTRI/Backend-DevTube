921c5964b2409dc5752443475a408b6e20d0db21
SAIRANTRI <pathaksairantri@gmail.com>
2024-08-02 11:43:05 +0530

    utilities added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
     path: "./env"
 })
 
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed! ", err);
+})
 
 // import express from "express"
 // const app = express();
@@ -29,4 +37,3 @@ dotenv.config({
 // })() //iifie way to create db(proffesional way)
 
 
-connectDB();
